65892648d68111c8c22c5f7dfcbf20572894b5f8
Anastasiia Kuzmych <51130742+Nastichichika@users.noreply.github.com>
2020-10-01 23:14:05 +0300

    update and start server

--- lab1/src/Server.java
+++ lab1/src/Server.java
@@ -1,16 +1,38 @@
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
 
 public class Server {
+    InetSocketAddress address;
+    int number;
 
-    public static void main(String[] args) throws Exception {
-        ServerSocket welcomeSocket = new ServerSocket(6789);
+    public Server(String host, int port, int number){
+        this.address = new InetSocketAddress(host, port);
+        this.number = number;
+    }
 
-        while(true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            
+    public void start() throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(9999));
+        serverSocketChannel.configureBlocking(false);
+
+        while(true){
+
+            SocketChannel socketChannel = serverSocketChannel.accept();
+
+            System.out.println("Server started :)");
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024*1024);
+            byteBuffer.put(Integer.toString(number).getBytes());
+            byteBuffer.flip();
+
+            socketChannel.write(byteBuffer);
+            //writing to client
+            //do something with socketChannel...
         }
     }
+
+
 }
